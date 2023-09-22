1b81dfbb9d5f3f63be0f5243954c48d419a0b77c
jeffreyjxang <ouraize2003@gmail.com>
2023-09-22 13:39:43 -0400

    Initial SQL Setup

--- src/UserEditor.java
+++ src/UserEditor.java
@@ -1,5 +1,6 @@
 public class UserEditor {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        MySQLAccess sql = new MySQLAccess();
+        sql.readDataBase();
     }
 }
