7800e34ab82c00c42bf9d7b394abd5ae61fdde6f
Mahim <80307451+MM120-i@users.noreply.github.com>
2023-10-12 21:24:05 -0400

    Update ProfileCreationWindow.java

--- src/PROJECT/ProfileCreationWindow.java
+++ src/PROJECT/ProfileCreationWindow.java
@@ -5,11 +5,13 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
+//Use case 1.
+
 public class ProfileCreationWindow extends JFrame {
 	
-    private static final long serialVersionUID = 1L;
-	
-    private JTextField dateOfBirthField, heightField, weightField;
+	private static final long serialVersionUID = 1L;
+	private JTextField dateOfBirthField, heightField, weightField;
     private JRadioButton maleRadioButton, femaleRadioButton;
     private ButtonGroup genderGroup;
     private JRadioButton metricRadioButton, imperialRadioButton;
@@ -19,15 +21,12 @@ public class ProfileCreationWindow extends JFrame {
     	
         setTitle("Profile Creation");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-      //  Color backgroundColor = new Color(30, 30, 30);
-        
+          
         JPanel mainPanel = new JPanel(new GridBagLayout());
-      //  mainPanel.setBackground(backgroundColor);
         GridBagConstraints cons = new GridBagConstraints();
         cons.fill = GridBagConstraints.BOTH; // Allow components to stretch both vertically and horizontally
         cons.insets = new Insets(5, 5, 5, 5);
-
+        
         cons.gridx = 0;
         cons.gridy = 0;
         JLabel gender = new JLabel("Gender:");
@@ -98,10 +97,12 @@ public class ProfileCreationWindow extends JFrame {
         cons.anchor = GridBagConstraints.CENTER;
 
         // Define weights for the "Create Profile" button to make it expand both vertically and horizontally.
-        cons.weightx = 1.0;
-        cons.weighty = 1.0;
+        cons.weightx = 0;
+        cons.weighty = 0;
+        
         JButton createProfileButton = new JButton("Create Profile");
-
+        createProfileButton.setPreferredSize(new Dimension(100, 30));
+        
         createProfileButton.addActionListener(new ActionListener() {
         	
             @Override
@@ -109,6 +110,30 @@ public class ProfileCreationWindow extends JFrame {
                 // Handle user inputs here:
             }
         });
+        
+        Font labelFont = new Font("Arial", Font.BOLD, 16);
+        Font buttonFont = new Font("Arial", Font.BOLD, 14);
+        
+        gender.setFont(labelFont);
+        maleRadioButton.setFont(buttonFont);
+        femaleRadioButton.setFont(buttonFont);
+        DOB.setFont(labelFont);
+        height.setFont(labelFont);
+        weight.setFont(labelFont);
+        unit.setFont(labelFont);
+        metricRadioButton.setFont(buttonFont);
+        imperialRadioButton.setFont(buttonFont);
+        createProfileButton.setFont(buttonFont);
+        
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        genderPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
+        unitsPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
+        createProfileButton.setBorder(BorderFactory.createLineBorder(new Color(0, 102, 204), 2));
+        
+        
+        mainPanel.setBackground(new Color(240, 240, 240));
+        createProfileButton.setForeground(Color.WHITE);
+        createProfileButton.setBackground(new Color(0, 102, 204));
 
         mainPanel.add(createProfileButton, cons);
 
@@ -117,6 +142,8 @@ public class ProfileCreationWindow extends JFrame {
         setSize(800, 600);
         setLocationRelativeTo(null);
         setVisible(true);
+        
+        
     }
 
     public static void main(String[] args) {
