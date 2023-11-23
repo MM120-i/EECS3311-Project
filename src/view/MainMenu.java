a8eae8874d30afffb6b43c14fe855294866cac13
Mahim <80307451+MM120-i@users.noreply.github.com>
2023-11-22 22:44:29 -0500

    Updated java doc and comments.

--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -7,7 +7,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * The type Main menu.
+ * The MainMenu class represents the main menu of the application, providing options for various features
+ * such as logging meals and exercises, estimating fat loss, tracking calories versus exercise, visualizing nutrient intake,
+ * and accessing Canada's Food Guide recommendations.
  */
 public class MainMenu extends JFrame{
     private JButton mealsButton;
@@ -24,30 +26,30 @@ public class MainMenu extends JFrame{
     /**
      * Instantiates a new Main menu.
      *
-     * @param uic the uic
+     * @param uic The UIController instance for handling user interface actions and interactions.
      */
     public MainMenu(UIController uic) {
-        mealsButton.addActionListener(new ActionListener() {
+        mealsButton.addActionListener(new ActionListener() {  // ActionListener for meal logging
             @Override
             public void actionPerformed(ActionEvent e) {
                 DietLogWindow dlw = new DietLogWindow(uic);
 
             }
         });
-        exerciseButton.addActionListener(new ActionListener() {
+        exerciseButton.addActionListener(new ActionListener() { // ActionListener for exercise logging
             @Override
             public void actionPerformed(ActionEvent e) {
                 ExerciseLogWindow elw = new ExerciseLogWindow(uic);
             }
         });
-        estimationsButton.addActionListener(new ActionListener() {
+        estimationsButton.addActionListener(new ActionListener() { // ActionListener for fat loss estimation
             @Override
             public void actionPerformed(ActionEvent e) {
                 FatLossEstimatorUI fleUI = new FatLossEstimatorUI(uic);
                 fleUI.showUI();
             }
         });
-        calorieVsExerciseButton.addActionListener(new ActionListener() {
+        calorieVsExerciseButton.addActionListener(new ActionListener() { // ActionListener for calorie versus exercise tracking
             @Override
             public void actionPerformed(ActionEvent e) {
                 CalorieTrackerUI ctUI = new CalorieTrackerUI(uic);
@@ -56,14 +58,14 @@ public class MainMenu extends JFrame{
         });
 
 
-        nutrientIntakeButton.addActionListener(new ActionListener() {
+        nutrientIntakeButton.addActionListener(new ActionListener() {  // ActionListener for nutrient intake visualization
             @Override
             public void actionPerformed(ActionEvent e) {
                 NutrientVisualizerUI nvUI = new NutrientVisualizerUI(uic);
                 nvUI.showUI();
             }
         });
-        button3.addActionListener(new ActionListener() {
+        button3.addActionListener(new ActionListener() { // ActionListener for accessing Canada's Food Guide recommendations
             @Override
             public void actionPerformed(ActionEvent e) {
                 CanadaFoodGuideUI cfgUI = new CanadaFoodGuideUI(uic);
